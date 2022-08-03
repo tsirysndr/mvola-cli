4b6ac366581e25f982f22776e729f4a8fb6d79ff
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2022-08-03 19:10:13 +0300

    docs: update page description

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="A Command-line tool for MVola API">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
